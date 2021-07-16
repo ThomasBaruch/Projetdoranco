4f0c1481cf527c2832ac3b776921e05a70398cf1
Nicosarace <87474169+Nicosarace@users.noreply.github.com>
2021-07-16 11:50:51 +0200

    Add files via upload

---
+++ update_employes.php
@@ -0,0 +1,17 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>update_employes</title>
+</head>
+<body>
+    <h1> TEST ! </h1>
+    <h2>(on se moque pas)git commit</h2>
+</body>
+<?php
+?>
+</html>
